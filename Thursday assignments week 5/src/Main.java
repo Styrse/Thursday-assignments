263aa8cca7e51a9118d423eca171ff2d6d4b3314
Styrse <styrbjoern@me.com>
2024-09-26 13:18:48 +0200

    Update

--- Thursday assignments week 5/src/Main.java
+++ Thursday assignments week 5/src/Main.java
@@ -2,5 +2,8 @@
 public class Main {
     public static void main(String[] args) {
 
+        //1. g
+        Team greenTeam = new Team("The Green Machine");
+
     }
 }
--- Thursday assignments week 5/src/Team.java
+++ Thursday assignments week 5/src/Team.java
@@ -1,2 +1,21 @@
+//1. b
 public class Team {
+
+    //1. c
+    String teamName;
+    //1. d
+    int rank;
+    //1. e
+    String teamPlayerNames;
+
+    //1. f
+    Team(String teamName){
+
+        this.teamName = teamName;
+    }
+
+    //1. h
+    void setRank(int rank){
+        this.rank = rank;
+    }
 }
