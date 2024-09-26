8efc60ca26550d8dc2b1ad2be8c36b790b052df9
Styrse <styrbjoern@me.com>
2024-09-26 15:22:10 +0200

    Update

--- Thursday assignments week 5/TaskTwo/src/Main.java
+++ Thursday assignments week 5/TaskTwo/src/Main.java
@@ -1,7 +1,17 @@
-importn
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       Scanner scanName = new Scanner(System.in);
+       System.out.print("Enter your name: ");
+
+       String name = scanName.nextLine();
+
+       System.out.print(name + "enter your age: ");
+       Scanner scanAge = new Scanner(System.in);
+
+       int age = scanAge.nextLine();
+
+       System.out.print(age);
     }
 }
