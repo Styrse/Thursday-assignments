cc8a5e5eff6d265bcd73a5e3b9bb568688cf0c7c
Styrse <styrbjoern@me.com>
2024-09-26 13:46:29 +0200

    Update Main.java

--- Thursday assignments week 5/src/Main.java
+++ Thursday assignments week 5/src/Main.java
@@ -11,5 +11,17 @@ public class Main {
         //1. j - check
         System.out.println(greenTeam.toString());
 
+        //1. k
+        String[] teams = {"Yellow thunder", "Silver Sharks", "Violet Vortex", "Pink Panthers", "Ruby Raptors"};
+        int[] teamsRank = {10, 7, 12, 3, 5};
+
+        Team[] team = new Team[teams.length];
+
+        for (int i = 0; i < teams.length; i++) {
+            team[i] = new Team(teams[i]);
+            team[i].setRank(teamsRank[i]);
+            System.out.println(team[i].toString());
+
+        }
     }
 }
