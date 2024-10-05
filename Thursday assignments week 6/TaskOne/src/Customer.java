437da7fb30a5d9eee257afe5e2877276bf1962e2
Styrse <styrbjoern@me.com>
2024-10-05 14:28:40 +0200

    Update
    TaskOne

--- Thursday assignments week 6/TaskOne/src/Customer.java
+++ Thursday assignments week 6/TaskOne/src/Customer.java
@@ -1,32 +1,48 @@
 public class Customer {
 
-    String firstname;
-    String lastname;
-    String username;
-    int ID;
-    int count;
+    private String firstname;
+    private String lastname;
+    private String username;
+    public int id;
 
-    Customer(String firstname, String lastname, String username){
+    private static int count = 1;
+
+    public Customer(String firstname, String lastname, String username) {
         this.firstname = firstname;
         this.lastname = lastname;
         this.username = username;
-        count++;
-        this.ID = count;
+        this.id = count++;
     }
 
-    String getFirstname(){
+    public String getFirstname(){
         return firstname;
     }
 
-    String getLastname(){
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname(){
         return lastname;
     }
 
-    String getUsername(){
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getUsername(){
         return username;
     }
 
-    int getID(){
-        return ID;
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String toString(){
+        return "Customer:" + "\nID: " + id + "\nName: " + firstname + " " + lastname + "\nUsername: " + username;
     }
 }
--- Thursday assignments week 6/TaskOne/src/Main.java
+++ Thursday assignments week 6/TaskOne/src/Main.java
@@ -1,7 +1,17 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 
+        ArrayList<Customer> customers = new ArrayList<Customer>();
 
+        customers.add(new Customer("Styrbjørn", "Gullacksen", "Styrse"));
+        customers.add(new Customer("Alessandra", "Sandoni", "Alex"));
+        customers.add(new Customer("Carl", "Gullacksen", "Carlo"));
+
+        for (Customer customer : customers) {
+            System.out.println(customer.toString());
+        }
 
     }
 }
