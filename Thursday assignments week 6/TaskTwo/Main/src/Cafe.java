1df5a4a946338572dee5ea424b97598b9271141e
Styrse <styrbjoern@me.com>
2024-10-07 16:49:07 +0200

    Update

---
+++ Thursday assignments week 6/TaskTwo/Main/src/Cafe.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Cafe {
+    private ArrayList<String> cofffeMenu = new ArrayList<>();
+
+
+    public void setCofffeMenu(ArrayList<String> cofffeMenu) {
+        this.cofffeMenu = cofffeMenu;
+    }
+}
