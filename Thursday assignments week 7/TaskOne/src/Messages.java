001becf79389fe62553342fcc7d137b482f61622
Styrse <styrbjoern@me.com>
2024-10-09 20:10:46 +0200

    Update

---
+++ Thursday assignments week 7/TaskOne/src/Messages.java
@@ -0,0 +1,22 @@
+//1. a & 1. b
+public class Messages {
+
+    void methodA(String start) {
+        if (start.equals("Start"))
+            System.out.println('J');
+    }
+
+    void methodB(){
+
+    }
+
+    void methodC(){
+
+    }
+
+    void methodD(){
+
+    }
+
+
+}
