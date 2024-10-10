80aaa3c60cf3b4ea2fddcc41b31b1d1d0253fbd8
Styrse <styrbjoern@me.com>
2024-10-10 12:34:13 +0200

    Update

--- Thursday assignments week 7/TaskOne/src/Main.java
+++ Thursday assignments week 7/TaskOne/src/Main.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Messages msg = new Messages();
+        System.out.println("Pick a cake: Strawberry or Carrot");
+        Scanner scan = new Scanner(System.in);
+        String choice = scan.nextLine();
+        msg.methodA(choice);
     }
 }
--- Thursday assignments week 7/TaskOne/src/Messages.java
+++ Thursday assignments week 7/TaskOne/src/Messages.java
@@ -1,24 +1,51 @@
 //1. a & 1. b
 public class Messages {
 
-    void methodA(String start) {
-        if (start.equals("Start"))
+    void methodA(String cake) {
+        if (cake.equals("Strawberry")) {
             System.out.println('J');
+        } else if (cake.equals("Carrot")) {
+            System.out.println('T');
+        }
+        methodB(cake);
     }
 
-    void methodB(){
+    void methodB(String cake) {
+        System.out.println('e');
+        if (cake.length() == 6) {
+            System.out.println('s');
+            if (cake.charAt(2) == 'r') {
+                System.out.println('s');
+            }
+        } else if (cake.length() <= 10 && cake.length() > 5) {
+            System.out.println('s');
+            System.out.println('p');
+            methodB("Red Velvet Cake");
+            methodD("Opera");
+        }
+        if (cake.equals("Strawberry") || cake.equals("Carrot")) {
+            System.out.println(' ');
+            methodC("Dream cake");
+        }
 
     }
 
-    void methodC(){
-
+    void methodC(String cake) {
+        if (cake.startsWith("D")) {
+            System.out.println('s');
+            System.out.println('h');
+        }
     }
 
-    void methodD(){
-
+    void methodD(String cake) {
+        if (cake.equals("Opera")) {
+            System.out.println('r');
+        } else if (cake != "Berlingozzo") {
+            methodC("Coffee");
+        }
     }
-
-
 }
 
 //Jesper/Tess should bring cake
+
+
