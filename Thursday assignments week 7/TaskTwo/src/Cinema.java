1a103021064608093cf16af9fbc0537441deb34f
Styrse <styrbjoern@me.com>
2024-10-10 22:07:03 +0200

    Update
    TaskTwo

--- Thursday assignments week 7/TaskTwo/src/Cinema.java
+++ Thursday assignments week 7/TaskTwo/src/Cinema.java
@@ -5,14 +5,12 @@ public class Cinema {
     // Sæder, der ikke er reserveret, har værdien "O"
     public Cinema(int rows, int seats) {
         this.seats = new String[rows][seats];
-        //Initialized the array seats
-        for (int i = 0; i < rows; i++){
-            for (int j = 0; j < seats; j++){
+        for (int i = 0; i < rows; i++) {                                                    //Initialized the array seats ***
+            for (int j = 0; j < seats; j++) {
                 this.seats[i][j] = "O";
             }
         }
         this.seats[0][0] = "X";
-
     }
 
     public int getRows() {
@@ -27,23 +25,31 @@ public class Cinema {
     // In that case, change the value to X and return true.
     // If the seat is already reserved, return false.
     public boolean reserve(int row, int seat) {
-        if (seats[row][seat].equals("O")) {
-            seats[row][seat] = "X";
-            return true;
+        if (row < getRows() + 1 && row > 0 && seat < getSeats() + 1 && seat > 0) {          //Added checker to avoid outOfBound ***
+            if (seats[row - 1][seat - 1].equals("O")) {                                     //added -1 ***
+                seats[row - 1][seat - 1] = "X";                                             //added -1 ***
+                return true;
+            }
+        } else {
+            System.out.println("Please enter a row number between 1 and " + getRows() + " and a seat between 1 and " + getSeats());
         }
         return false;
     }
 
     public boolean cancelReservation(int row, int seat) {
-        if (seats[row][seat].equals("x")) {
-            seats[row][seat] = "O";
-            return true;
+        if (row < getRows() + 1 && row > 0 && seat < getSeats() + 1 && seat > 0) {          //Added checker to avoid outOfBound ***
+            if (seats[row - 1][seat - 1].equals("X")) {                                     //added -1  and capitalized "X" ***
+                seats[row - 1][seat - 1] = "O";                                             //added -1 ***
+                return true;
+            }
+        } else {
+            System.out.println("Please enter a row number between 1 and " + getRows() + " and a seat between 1 and " + getSeats());
         }
         return false;
     }
 
     public String toString() {
-        String result = null;
+        String result = "";                                                                 //Changed to "" instead of null ***
         for (int i = 0; i < seats.length; i++) {
             result += "|";
             for (int j = 0; j < seats[i].length; j++) {
--- Thursday assignments week 7/TaskTwo/src/FindFemFejl.java
+++ Thursday assignments week 7/TaskTwo/src/FindFemFejl.java
@@ -13,7 +13,7 @@ public class FindFemFejl {
         // Reserver det sidste sæde i den sidste række
         int numberOfRows = biogrande.getRows();
         int numberOfSeats = biogrande.getSeats();
-         biogrande.reserve(numberOfRows, numberOfSeats);
+        biogrande.reserve(numberOfRows, numberOfSeats);                                     //Fixed indentation/space (didn't cause any issues) ***
 
         // Afbestil reservation på række 1, sæde 5
         System.out.println("Række 1, sæde 5 er afbestilt: " + biogrande.cancelReservation(1, 5));
